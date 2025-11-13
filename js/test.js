2cc36e5252724b3abfae49ab6f865aec0332edd1
inhamostova <inha.mostova@gmail.com>
2025-11-13 16:24:21 +0200

    Update test.js

--- js/test.js
+++ js/test.js
@@ -67,4 +67,67 @@
 //     console.log('incorrect')
 // }
 
+// let counter = 0;
+
+// while (counter < 10) {
+//     console.log(counter);
+//     counter += 1;
+// }
+
+// let clientCounter = 18;
+// const maxClients = 25;
+
+// while (clientCounter < maxClients) {
+//     clientCounter += 1;
+//     console.log(clientCounter);
+    
+// }
+
+// let password = '';
+
+// do {
+//     password = prompt('Your password must have at least 4 symbols');
+
+// } while (password.length < 4);
+
+// console.log(password);
+
+// for (let i = 0; i <= 20; i += 5) {
+//     console.log(i);
+    
+// }
+
+// const target = 3;
+// let sum = 0;
+
+// for (let i = 0; i <= target; i += 1) {
+//     sum += i;
+//     console.log(sum);
+// }
+
+// const max = 10;
+// for (let i = 0; i < max; i += 1) {
+//   console.log(`${max} % ${i} = `, max % i);
+// }
+
+// for (let i = 0; i <= 5; i += 1) {
+//     if (i === 3) {
+//         console.log('found number 3');
+//         break
+//     }
+    
+// }
+
+// console.log("log after cycle");
+
+// let odd;
+
+// for (let i = 0; i <= 10; i += 1) {
+//     if (i % 2 !== 0) {
+//         odd = i;
+//     console.log(odd);
+//     } 
+// }
+
+
 
