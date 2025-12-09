4a739ca512ed3e240b91f4420e24e22bd58740fb
inhamostova <inha.mostova@gmail.com>
2025-12-09 22:17:16 +0200

    Update train.js

--- js/train.js
+++ js/train.js
@@ -137,85 +137,85 @@
 // console.log(changeEven([1, 2, 3, 4, 5], 10));
 // console.log(changeEven([17, 24, 68, 31, 42], 100));
 
-const people = [
-  {
-    name: 'Moore Hensley',
-    email: 'moorehensley@indexia.com',
-    eyeColor: 'blue',
-    friends: ['Sharron Pace'],
-    isActive: false,
-    balance: 2811,
-    skills: ['ipsum', 'lorem'],
-    gender: 'male',
-    age: 37,
-  },
-  {
-    name: 'Sharlene Bush',
-    email: 'sharlenebush@tubesys.com',
-    eyeColor: 'blue',
-    friends: ['Briana Decker', 'Sharron Pace'],
-    isActive: true,
-    balance: 3821,
-    skills: ['tempor', 'mollit', 'commodo', 'veniam', 'laborum'],
-    gender: 'female',
-    age: 34,
-  },
-  {
-    name: 'Ross Vazquez',
-    email: 'rossvazquez@xinware.com',
-    eyeColor: 'green',
-    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
-    isActive: false,
-    balance: 3793,
-    skills: ['nulla', 'anim', 'proident', 'ipsum', 'elit'],
-    gender: 'male',
-    age: 24,
-  },
-  {
-    name: 'Elma Head',
-    email: 'elmahead@omatom.com',
-    eyeColor: 'green',
-    friends: ['Goldie Gentry', 'Aisha Tran'],
-    isActive: true,
-    balance: 2278,
-    skills: ['adipisicing', 'irure', 'velit'],
-    gender: 'female',
-    age: 21,
-  },
-  {
-    name: 'Carey Barr',
-    email: 'careybarr@nurali.com',
-    eyeColor: 'blue',
-    friends: ['Jordan Sampson', 'Eddie Strong'],
-    isActive: true,
-    balance: 3951,
-    skills: ['ex', 'culpa', 'nostrud'],
-    gender: 'male',
-    age: 27,
-  },
-  {
-    name: 'Blackburn Dotson',
-    email: 'blackburndotson@furnigeer.com',
-    eyeColor: 'brown',
-    friends: ['Jacklyn Lucas', 'Linda Chapman'],
-    isActive: false,
-    balance: 1498,
-    skills: ['non', 'amet', 'ipsum'],
-    gender: 'male',
-    age: 38,
-  },
-  {
-    name: 'Sheree Anthony',
-    email: 'shereeanthony@kog.com',
-    eyeColor: 'brown',
-    friends: ['Goldie Gentry', 'Briana Decker'],
-    isActive: true,
-    balance: 2764,
-    skills: ['lorem', 'veniam', 'culpa'],
-    gender: 'female',
-    age: 39,
-  },
-];
+// const people = [
+//   {
+//     name: 'Moore Hensley',
+//     email: 'moorehensley@indexia.com',
+//     eyeColor: 'blue',
+//     friends: ['Sharron Pace'],
+//     isActive: false,
+//     balance: 2811,
+//     skills: ['ipsum', 'lorem'],
+//     gender: 'male',
+//     age: 37,
+//   },
+//   {
+//     name: 'Sharlene Bush',
+//     email: 'sharlenebush@tubesys.com',
+//     eyeColor: 'blue',
+//     friends: ['Briana Decker', 'Sharron Pace'],
+//     isActive: true,
+//     balance: 3821,
+//     skills: ['tempor', 'mollit', 'commodo', 'veniam', 'laborum'],
+//     gender: 'female',
+//     age: 34,
+//   },
+//   {
+//     name: 'Ross Vazquez',
+//     email: 'rossvazquez@xinware.com',
+//     eyeColor: 'green',
+//     friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+//     isActive: false,
+//     balance: 3793,
+//     skills: ['nulla', 'anim', 'proident', 'ipsum', 'elit'],
+//     gender: 'male',
+//     age: 24,
+//   },
+//   {
+//     name: 'Elma Head',
+//     email: 'elmahead@omatom.com',
+//     eyeColor: 'green',
+//     friends: ['Goldie Gentry', 'Aisha Tran'],
+//     isActive: true,
+//     balance: 2278,
+//     skills: ['adipisicing', 'irure', 'velit'],
+//     gender: 'female',
+//     age: 21,
+//   },
+//   {
+//     name: 'Carey Barr',
+//     email: 'careybarr@nurali.com',
+//     eyeColor: 'blue',
+//     friends: ['Jordan Sampson', 'Eddie Strong'],
+//     isActive: true,
+//     balance: 3951,
+//     skills: ['ex', 'culpa', 'nostrud'],
+//     gender: 'male',
+//     age: 27,
+//   },
+//   {
+//     name: 'Blackburn Dotson',
+//     email: 'blackburndotson@furnigeer.com',
+//     eyeColor: 'brown',
+//     friends: ['Jacklyn Lucas', 'Linda Chapman'],
+//     isActive: false,
+//     balance: 1498,
+//     skills: ['non', 'amet', 'ipsum'],
+//     gender: 'male',
+//     age: 38,
+//   },
+//   {
+//     name: 'Sheree Anthony',
+//     email: 'shereeanthony@kog.com',
+//     eyeColor: 'brown',
+//     friends: ['Goldie Gentry', 'Briana Decker'],
+//     isActive: true,
+//     balance: 2764,
+//     skills: ['lorem', 'veniam', 'culpa'],
+//     gender: 'female',
+//     age: 39,
+//   },
+// ];
 
 // const getSortedFriends = users =>
 //   users
@@ -223,15 +223,15 @@ const people = [
 //     .filter((friend, idx, arr) => arr.indexOf(friend) === idx)
 //         .sort((a, b) => a.localeCompare(b));
 
-const getTotalBalanceByGender = (users, gender) => {
-  return users
-    .filter(user => user.gender === gender)
-    .reduce((acc, us) => {
-      return acc + us.balance;
-    }, 0);
-};
+// const getTotalBalanceByGender = (users, gender) => {
+//   return users
+//     .filter(user => user.gender === gender)
+//     .reduce((acc, us) => {
+//       return acc + us.balance;
+//     }, 0);
+// };
 
-console.log(getTotalBalanceByGender(people, 'female'));
+// console.log(getTotalBalanceByGender(people, 'female'));
 // const getUserNames = users => users.map(user => user.name);
 
 // const getUserEmails = users => users.map(user => user.email);
@@ -308,36 +308,36 @@ console.log(getTotalBalanceByGender(people, 'female'));
 
 // console.log(totalAveragePlaytimePerGame);
 
-const books = [
-  {
-    title: 'The Last Kingdom',
-    author: 'Bernard Cornwell',
-    rating: 8.38,
-  },
-  {
-    title: 'Beside Still Waters',
-    author: 'Robert Sheckley',
-    rating: 8.51,
-  },
-  {
-    title: 'The Dream of a Ridiculous Man',
-    author: 'Fyodor Dostoevsky',
-    rating: 7.75,
-  },
-  { title: 'Redder Than Blood', author: 'Tanith Lee', rating: 7.94 },
-  {
-    title: 'The Dreams in the Witch House',
-    author: 'Howard Lovecraft',
-    rating: 8.67,
-  },
-];
-const MIN_BOOK_RATING = 8;
+// const books = [
+//   {
+//     title: 'The Last Kingdom',
+//     author: 'Bernard Cornwell',
+//     rating: 8.38,
+//   },
+//   {
+//     title: 'Beside Still Waters',
+//     author: 'Robert Sheckley',
+//     rating: 8.51,
+//   },
+//   {
+//     title: 'The Dream of a Ridiculous Man',
+//     author: 'Fyodor Dostoevsky',
+//     rating: 7.75,
+//   },
+//   { title: 'Redder Than Blood', author: 'Tanith Lee', rating: 7.94 },
+//   {
+//     title: 'The Dreams in the Witch House',
+//     author: 'Howard Lovecraft',
+//     rating: 8.67,
+//   },
+// ];
+// const MIN_BOOK_RATING = 8;
 // Change code below this line
 
-const names = books
-  .filter(book => book.rating > MIN_BOOK_RATING)
-  .map(book => book.author)
-  .sort((a, b) => a.localeCompare(b));
+// const names = books
+//   .filter(book => book.rating > MIN_BOOK_RATING)
+//   .map(book => book.author)
+//   .sort((a, b) => a.localeCompare(b));
 // Change code below this line
 // console.log(names);
 
@@ -350,3 +350,147 @@ const names = books
 // const sortedByDescentingRating = [...books].sort((a, b) => b.rating - a.rating);
 
 // console.log(sortedByAscendingRating);
+
+// const chopShop = {
+//   stones: [
+//     {
+//       name: 'Emerald',
+//       price: 1300,
+//       quantity: 4,
+//     },
+//     {
+//       name: 'Diamond',
+//       price: 2700,
+//       quantity: 3,
+//     },
+//     {
+//       name: 'Sapphire',
+//       price: 1400,
+//       quantity: 7,
+//     },
+//     {
+//       name: 'Ruby',
+//       price: 800,
+//       quantity: 2,
+//     },
+//   ],
+
+//   calcTotalPrice(stoneName) {
+//     // let total;
+//     // this.stones.forEach(({ name, price, quantity }) => {
+//     //   if (name === stoneName) {
+//     //     total = price * quantity;
+//     //   }
+//     // });
+
+//     // return total;
+
+//     // return this.stones.reduce(
+//     //   (acc, { name, price, quantity }) => (name === stoneName ? price * quantity : acc),
+//     //   0
+//     // );
+
+//     const obj = this.stones.find(({ name }) => name === stoneName);
+//     const { price, quantity } = obj;
+//     return price * quantity;
+//   },
+// };
+
+// console.log(chopShop.calcTotalPrice('Emerald')); //5200
+// console.log(chopShop.calcTotalPrice('Diamond')); //8100
+// console.log(chopShop.calcTotalPrice('Sapphire')); //9800
+
+const phonebook = {
+  contacts: [],
+  add(contact) {
+    const generateId = () => {
+      return '_' + Math.random().toString(36).substr(2, 9);
+    };
+
+    const getDate = () => {
+      return Date.now();
+    };
+    const newContact = {
+      list: 'default',
+      ...contact,
+      id: generateId(),
+      createdAt: getDate(),
+    };
+
+    this.contacts.push(newContact);
+    return this.contacts;
+  },
+
+  // generateId() {
+  //   return '_' + Math.random().toString(36).substr(2, 9);
+  // },
+
+  // getDate() {
+  //   return Date.now();
+  // },
+};
+
+// const phonebook1 = {
+//   contacts: [],
+//   add(contact) {
+//     const newContact = {
+//       list: 'default',
+//       ...contact,
+//       id: this.generateId(),
+//       createdAt: this.getDate(),
+//     };
+
+//     this.contacts.push(newContact);
+//     return this.contacts;
+//   },
+
+//   generateId() {
+//     return '_' + Math.random().toString(36).substr(2, 9);
+//   },
+
+//   getDate() {
+//     return Date.now();
+//   },
+// };
+
+// console.log(phonebook.add({ name: 'Mango', list: 'friends', email: 'mango@mail.com' }));
+
+const newPhoneBook = {
+  name: 'NEW',
+  contacts: [],
+};
+
+phonebook.add.call(newPhoneBook, {
+  name: 'Mango',
+  list: 'friends',
+  email: 'mango@mail.com',
+});
+
+console.log(newPhoneBook);
+
+// const calculator = {
+//   create(a, b) {
+//     this.a = a;
+//     this.b = b;
+
+//     // const add = () => this[a] + this[b];
+//     // console.log(add());
+
+//     // const mult = () => this[a] * this[b];
+//     // console.log(mult());
+//   },
+
+//   add() {
+//     return this.a + this.b;
+//   },
+
+//   mult() {
+//     return this.a * this.b;
+//   },
+// };
+
+// calculator.create(29, 352);
+// console.log(calculator);
+// calculator.create(7, 2);
+// console.log(calculator.add());
+// console.log(calculator.mult());
