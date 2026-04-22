26a09fbe8eb7121ad087168920696c264670d4bc
inhamostova <inha.mostova@gmail.com>
2026-04-22 21:31:20 +0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -113,23 +113,23 @@
 //   }
 // }
 
-console.log(getUniqueValues([{ id: 1 }, { id: 1 }, { id: 2 }]));
+// console.log(getUniqueValues([{ id: 1 }, { id: 1 }, { id: 2 }]));
 
-// console.log(getUniqueValues([1, 2, 2, 3, 4, 4]));
-// → [1, 2, 3, 4]
+// // console.log(getUniqueValues([1, 2, 2, 3, 4, 4]));
+// // → [1, 2, 3, 4]
 
-function getUniqueValues(arr) {
-  const seen = new Set();
-  console.log(seen);
+// function getUniqueValues(arr) {
+//   const seen = new Set();
+//   console.log(seen);
 
-  return arr.filter(item => {
-    if (seen.has(item.id)) {
-      return false;
-    }
-    seen.add(item.id); // запам’ятали
-    return true;
-  });
-}
+//   return arr.filter(item => {
+//     if (seen.has(item.id)) {
+//       return false;
+//     }
+//     seen.add(item.id); // запам’ятали
+//     return true;
+//   });
+// }
 
 // console.log(countOccurrences(['a', 'b', 'a', 'c', 'b', 'a']));
 // // → { a: 3, b: 2, c: 1 }
@@ -153,16 +153,223 @@ function getUniqueValues(arr) {
 //   return result;
 // }
 
-groupBy([
-  { type: 'fruit', name: 'apple' },
-  { type: 'veg', name: 'carrot' },
-  { type: 'fruit', name: 'banana' },
-]);
-// {
-//   fruit: [{...}, {...}],
-//   veg: [{...}]
+// groupBy([
+//   { type: 'fruit', name: 'apple' },
+//   { type: 'veg', name: 'carrot' },
+//   { type: 'fruit', name: 'banana' },
+// ]);
+// // {
+// //   fruit: [{...}, {...}],
+// //   veg: [{...}]
+// // }
+
+// function groupBy(arr) {
+//   return arr.reduce((acc, { type, name }) => {}, { fruit: [], veg: [] });
 // }
 
-function groupBy(arr) {
-  return arr.reduce((acc, { type, name }) => {}, { fruit: [], veg: [] });
+// console.log(sum([1, 2, 3, 4])); // 10
+
+// function sum(arr) {
+//   return arr.reduce((acc, item) => acc + item, 0);
+// }
+
+// console.log(findMax([3, 7, 2, 9])); // 9;
+
+// function findMax(arr) {
+//   let max = arr[0];
+
+//   for (let i = 0; i < arr.length; i += 1) {
+//     max = arr[i] > max ? arr[i] : max;
+//   }
+
+//   return max;
+// }
+
+// console.log(getEvenNumbers([1, 2, 3, 4, 5])); // [2, 4]
+
+// function getEvenNumbers(arr) {
+//   return arr.reduce((acc, item) => {
+//     if (item % 2 === 0) {
+//       acc.push(item);
+//     }
+//     return acc;
+//   }, []);
+// }
+
+// console.log(getLengths(['hi', 'hello'])); // [2, 5]
+
+// function getLengths(arr) {
+//   return arr.map(item => item.length);
+// }
+
+// console.log(findLongWord(['cat', 'house', 'car'], 4)); // 'house'
+
+// function findLongWord(arr, maxLength) {
+//   return arr.find(item => item.length > maxLength);
+// }
+
+// console.log(countPositive([1, -2, 3, 0, -5])); // 2
+
+// function countPositive(arr) {
+//   return arr.filter(item => item > 0).reduce((acc, item) => acc + item, 0);
+// }
+
+// function countPositive(arr) {
+//   return arr.filter(item => item > 0).length;
+// }
+
+// console.log(countVowels('javascript')); // 3
+
+// function countVowels(string) {
+//   const vowelList = 'aeiou';
+
+//   return string.split('').filter(char => vowelList.includes(char.toLowerCase())).length;
+// }
+
+// console.log(reverseString('hello')); // 'olleh'
+
+// function reverseString(str) {
+//   return str.split('').reverse().join('');
+// }
+
+// console.log(isPalindrome('level')); // true
+// console.log(isPalindrome('Mad am')); // true
+
+// function isPalindrome(str) {
+//   const normalizedStr = str.toLowerCase().replaceAll(' ', '');
+//   const reverseStr = normalizedStr.split('').reverse().join('');
+
+//   return normalizedStr === reverseStr;
+// }
+
+// console.log(capitalize('hello world'));
+// // 'Hello World'
+
+// function capitalize(str) {
+//   return str
+//     .split(' ')
+//     .map(item => item[0].toUpperCase() + item.slice(1))
+//     .join(' ');
+// }
+
+// console.log(getCommon([1, 2, 3], [2, 3, 4]));
+// [2, 3]
+
+// function getCommon(arr1, arr2) {
+//   const commonArr = [];
+
+//   for (let i = 0; i < arr1.length; i += 1) {
+//     if (arr2.includes(arr1[i])) {
+//       commonArr.push(arr1[i]);
+//     }
+//   }
+
+//   return commonArr;
+// }
+
+// console.log(getUnique([1, 2, 2, 3, 4, 4]));
+// [1, 2, 3, 4]
+
+// function getUnique(arr) {
+//   return [...new Set(arr)];
+// }
+// console.log(hasDuplicates([1, 2, 3])); // false
+// console.log(hasDuplicates([1, 2, 2])); // true
+
+// function hasDuplicates(arr) {
+//   const set = new Set(arr);
+//   return arr.length !== set.size;
+// }
+
+// console.log(getCommon([1, 2, 3], [2, 3, 4]));
+// [2, 3]
+
+// function getCommon(arr1, arr2) {
+//   const set = new Set(arr1);
+
+//   return arr2.filter(item => set.has(item));
+// }
+
+// console.log(getUniqueChars('hello'));
+// ['h', 'e', 'l', 'o']
+
+// function getUniqueChars(str) {
+//   return [...new Set(str)];
+// }
+
+// console.log(removeDuplicates([1, 2, 2, 3, 4, 4]));
+// [1, 2, 3, 4]
+// function removeDuplicates(arr) {
+//   const newArr = [];
+
+//   for (let i = 0; i < arr.length; i += 1) {
+//     if (arr[i] === arr[i + 1]) {
+//       continue;
+//     }
+//     newArr.push(arr[i]);
+//   }
+
+//   return newArr;
+// }
+
+// function removeDuplicates(arr) {
+//   for (let i = 0; i < arr.length; i += 1) {
+//     if (arr[i] !== arr[i + 1]) {
+//       continue;
+//     }
+//     arr.splice(i, 1);
+//     i -= 1;
+//   }
+
+//   return arr;
+// }
+
+// [1, 2, 3, 4]
+
+// function flatten(arr) {
+//   const newArr = [];
+//   for (let i = 0; i < arr.length; i += 1) {
+//     if (!Array.isArray(arr[i])) {
+//       newArr.push(arr[i]);
+//     } else {
+//       newArr.push(...flatten(arr[i]));
+//     }
+//   }
+//   return newArr;
+// }
+
+// console.log(flatten([1, [2, 3], 4]));
+
+console.log(findFirstUnique([2, 3, 4, 2, 3, 5]));
+console.log(findFirstUnique([2, 3, 4, 4, 5, 2, 3, 5]));
+console.log(findFirstUnique([2, 17, 99, 22, 3, 4, 4, 17, 99, 2, 3, 7, 5]));
+
+// 4
+
+// function findFirstUnique(arr) {
+//   const myMap = new Map();
+
+//   for (const num of arr) {
+//     myMap.set(num, (myMap.get(num) ?? 0) + 1);
+//   }
+
+//   for (const num of arr) {
+//     if (myMap.get(num) === 1) {
+//       return num;
+//     }
+//   }
+//   return false;
+// }
+
+function findFirstUnique(arr) {
+  const counts = {};
+
+  for (const num of arr) {
+    counts[num] = (counts[num] ?? 0) + 1;
+  }
+
+  for (const num of arr) {
+    if (counts[num] === 1) return num;
+  }
+  return null;
 }
