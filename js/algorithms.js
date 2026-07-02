e140915833658a7c374ca18ea8f15cc5a0a1eece
inhamostova <inha.mostova@gmail.com>
2026-07-02 20:19:29 +0300

    commit123

--- js/algorithms.js
+++ js/algorithms.js
@@ -16,24 +16,61 @@
 //   return result;
 // }
 
-console.log(productOfArrayExceptSelf([1, 2, 3, 4])); //[24, 12, 8, 6]
-console.log(productOfArrayExceptSelf([2, 5, 1])); //[5,2,10]
+// console.log(productOfArrayExceptSelf([1, 2, 3, 4])); //[24, 12, 8, 6]
+// console.log(productOfArrayExceptSelf([2, 5, 1])); //[5,2,10]
 
-function productOfArrayExceptSelf(arr) {
-  const result = [];
-  let product = 1;
+// function productOfArrayExceptSelf(arr) {
+//   const result = [];
+//   let product = 1;
 
-  for (let i = 0; i < arr.length; i += 1) {
-    result[i] = product;
-    product *= arr[i];
-  }
+//   for (let i = 0; i < arr.length; i += 1) {
+//     result[i] = product;
+//     product *= arr[i];
+//   }
 
-  product = 1;
+//   product = 1;
 
-  for (let i = arr.length - 1; i >= 0; i -= 1) {
-    result[i] = result[i] * product;
-    product *= arr[i];
-  }
+//   for (let i = arr.length - 1; i >= 0; i -= 1) {
+//     result[i] = result[i] * product;
+//     product *= arr[i];
+//   }
 
-  return result;
-}
+//   return result;
+// }
+
+// Will run first
+// setTimeout(() => {
+//   // Will run last, after 2000 milliseconds
+//   console.log('A');
+// }, 2000);
+
+// console.log('B');
+
+// setTimeout(() => {
+//   // Will run last, after 2000 milliseconds
+//   console.log('C');
+// }, 1500);
+// // Will run second
+// console.log('D');
+
+// const timerId = setInterval(() => {
+//   console.log('Tick');
+// }, 2000);
+
+// setTimeout(() => {
+//   clearInterval(timerId);
+// }, 5000);
+
+// for (let i = 3; i > 0; i -= 1) {
+//   const delay = i * 1000;
+//   setTimeout(() => {
+//     console.log(i);
+//   }, delay);
+// }
+
+// const date = new Date('2030-08-23');
+// console.log(date.getMonth());
+
+const start = new Date('2030-04-23');
+const end = new Date('2030-05-17');
+console.log(end - start);
